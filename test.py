18cedcaaf008354cbc52b78e29ac064e3547ce84
RUSSELL MATSON <39438491+russmatson@users.noreply.github.com>
2019-01-26 08:48:50 -0800

    Update test.py
    Added info for 3 day course outline for Feb.

--- test.py
+++ test.py
@@ -3,6 +3,88 @@
 3-4 day python bootcamp sylabus and the 12 week bootcamp sylybus to break this down into chunks and see
 what I am actually learning.  
 
+#Course Outline
+
+1. Python Basics
+Running Python
+Hello, World!
+Literals
+Python Comments
+Data Types
+Variables
+Writing a Python Module
+print() Function
+Named Arguments
+Collecting User Input
+Getting Help
+
+2. Functions and Modules
+Defining Functions
+Variable Scope
+Global Variables
+Function Parameters
+Returning Values
+Importing Modules
+
+3. Math
+Arithmetic Operators
+Modulus and Floor Division
+Assignment Operators
+Built-in Math Functions
+The math Module
+The random Module
+Seeding
+
+4. Python Strings
+Quotation Marks and Special Characters
+String Indexing
+Slicing Strings
+Concatenation and Repetition
+Common String Methods
+String Formatting
+Built-in String Functions
+
+5. Iterables: Sequences, Dictionaries, and Sets
+Definitions
+Sequences
+Unpacking Sequences
+Dictionaries
+The len() Function
+Sets
+*args and **kwargs
+
+6. Flow Control
+Conditional Statements
+The is and is not Operators
+Python's Ternary Operator
+Loops in Python
+The enumerate() Function
+Generators
+List Comprehensions
+
+7. File Processing
+Opening Files
+The os and os.path Modules
+
+8. Exception Handling
+Wildcard except Clauses
+Getting Information on Exceptions
+The else Clause
+The finally Clause
+Using Exceptions for Flow Control
+Exception Hierarchy
+
+9. Dates and Times
+Understanding Time
+The time Module
+The datetime Module
+
+10. Running Python Scripts from the Comand Line
+The sys Module
+sys.argv
+View outline in Word 
+
+
 x = 1 
 
 y = 2
